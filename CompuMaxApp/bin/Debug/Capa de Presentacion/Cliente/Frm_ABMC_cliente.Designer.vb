d3dd2ce60a394f5590ef8bae7bb9114ba004e755
Facundo Forcada <31261216+FacundoForcada@users.noreply.github.com>
2017-11-07 20:43:20 -0300

    Add files via upload

---
+++ CompuMaxApp/bin/Debug/Capa de Presentacion/Cliente/Frm_ABMC_cliente.Designer.vb
@@ -0,0 +1,222 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Frm_ABMC_cliente
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Frm_ABMC_cliente))
+        Me.cmb_tipo_doc = New System.Windows.Forms.ComboBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txt_nro_doc = New System.Windows.Forms.TextBox()
+        Me.txt_telefono = New System.Windows.Forms.MaskedTextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txt_apellido = New System.Windows.Forms.TextBox()
+        Me.btn_aceptar = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txt_mail = New System.Windows.Forms.MaskedTextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txt_nombre = New System.Windows.Forms.TextBox()
+        Me.txt_cuit = New System.Windows.Forms.TextBox()
+        Me.btn_crear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'cmb_tipo_doc
+        '
+        Me.cmb_tipo_doc.FormattingEnabled = True
+        Me.cmb_tipo_doc.Location = New System.Drawing.Point(278, 112)
+        Me.cmb_tipo_doc.Name = "cmb_tipo_doc"
+        Me.cmb_tipo_doc.Size = New System.Drawing.Size(65, 21)
+        Me.cmb_tipo_doc.TabIndex = 39
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(45, 84)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(28, 13)
+        Me.Label7.TabIndex = 49
+        Me.Label7.Text = "Cuit:"
+        '
+        'txt_nro_doc
+        '
+        Me.txt_nro_doc.Location = New System.Drawing.Point(90, 112)
+        Me.txt_nro_doc.Name = "txt_nro_doc"
+        Me.txt_nro_doc.Size = New System.Drawing.Size(107, 20)
+        Me.txt_nro_doc.TabIndex = 38
+        '
+        'txt_telefono
+        '
+        Me.txt_telefono.Location = New System.Drawing.Point(90, 174)
+        Me.txt_telefono.Name = "txt_telefono"
+        Me.txt_telefono.Size = New System.Drawing.Size(253, 20)
+        Me.txt_telefono.TabIndex = 42
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(16, 53)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(57, 13)
+        Me.Label6.TabIndex = 48
+        Me.Label6.Text = "Apellido(*):"
+        '
+        'txt_apellido
+        '
+        Me.txt_apellido.Location = New System.Drawing.Point(90, 50)
+        Me.txt_apellido.Name = "txt_apellido"
+        Me.txt_apellido.Size = New System.Drawing.Size(253, 20)
+        Me.txt_apellido.TabIndex = 35
+        '
+        'btn_aceptar
+        '
+        Me.btn_aceptar.Location = New System.Drawing.Point(151, 211)
+        Me.btn_aceptar.Name = "btn_aceptar"
+        Me.btn_aceptar.Size = New System.Drawing.Size(100, 25)
+        Me.btn_aceptar.TabIndex = 43
+        Me.btn_aceptar.Text = "&Aceptar"
+        Me.btn_aceptar.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(21, 177)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(52, 13)
+        Me.Label5.TabIndex = 47
+        Me.Label5.Text = "Telefono:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(205, 116)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(67, 13)
+        Me.Label4.TabIndex = 46
+        Me.Label4.Text = "Tipo Doc.(*):"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(13, 116)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(60, 13)
+        Me.Label3.TabIndex = 45
+        Me.Label3.Text = "Nro Doc(*):"
+        '
+        'txt_mail
+        '
+        Me.txt_mail.Location = New System.Drawing.Point(90, 143)
+        Me.txt_mail.Name = "txt_mail"
+        Me.txt_mail.Size = New System.Drawing.Size(253, 20)
+        Me.txt_mail.TabIndex = 40
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(38, 146)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 41
+        Me.Label2.Text = "Email:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 13)
+        Me.Label1.TabIndex = 37
+        Me.Label1.Text = "Nombre(*):"
+        '
+        'txt_nombre
+        '
+        Me.txt_nombre.Location = New System.Drawing.Point(90, 21)
+        Me.txt_nombre.Name = "txt_nombre"
+        Me.txt_nombre.Size = New System.Drawing.Size(253, 20)
+        Me.txt_nombre.TabIndex = 34
+        '
+        'txt_cuit
+        '
+        Me.txt_cuit.Location = New System.Drawing.Point(90, 81)
+        Me.txt_cuit.Name = "txt_cuit"
+        Me.txt_cuit.Size = New System.Drawing.Size(253, 20)
+        Me.txt_cuit.TabIndex = 36
+        '
+        'btn_crear
+        '
+        Me.btn_crear.Location = New System.Drawing.Point(257, 211)
+        Me.btn_crear.Name = "btn_crear"
+        Me.btn_crear.Size = New System.Drawing.Size(89, 25)
+        Me.btn_crear.TabIndex = 50
+        Me.btn_crear.Text = "&Crear"
+        Me.btn_crear.UseVisualStyleBackColor = True
+        Me.btn_crear.Visible = False
+        '
+        'Frm_ABMC_cliente
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(97, Byte), Integer), CType(CType(121, Byte), Integer), CType(CType(199, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(358, 252)
+        Me.Controls.Add(Me.btn_crear)
+        Me.Controls.Add(Me.txt_cuit)
+        Me.Controls.Add(Me.cmb_tipo_doc)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.txt_nro_doc)
+        Me.Controls.Add(Me.txt_telefono)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txt_apellido)
+        Me.Controls.Add(Me.btn_aceptar)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txt_mail)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txt_nombre)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Frm_ABMC_cliente"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Carga de Cliente"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents cmb_tipo_doc As System.Windows.Forms.ComboBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents txt_nro_doc As System.Windows.Forms.TextBox
+    Friend WithEvents txt_telefono As System.Windows.Forms.MaskedTextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents txt_apellido As System.Windows.Forms.TextBox
+    Friend WithEvents btn_aceptar As System.Windows.Forms.Button
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txt_mail As System.Windows.Forms.MaskedTextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txt_nombre As System.Windows.Forms.TextBox
+    Friend WithEvents txt_cuit As System.Windows.Forms.TextBox
+    Friend WithEvents btn_crear As System.Windows.Forms.Button
+End Class
